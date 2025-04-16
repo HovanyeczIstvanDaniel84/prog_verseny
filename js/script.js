49c1e7e74a93dcc6f3dc2daddb5194dc8215d662
DeCaroLorenzo <de.caro.lorenzo.2021i@bankitatabanya.hu>
2025-04-16 12:09:11 +0200

    js importalasa

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ async function fetchData() {
     return await (await fetch(api)).json();
 }
 
-async function JSONtoObject(json) {
+function JSONtoObject(json) {
     return new Element(json.nev, json.megrendelo, json.meret, json.szinek, json.darabszam, json.papirminoseg, json.kivetelezes, json.tema, json.kep);
 }
 
@@ -15,7 +15,9 @@ function displayElement(el) {
 
 async function init() {
     for (let el of await fetchData()) {
+
         const obj = JSONtoObject(el);
+        console.log(obj)
         displayElement(obj);
     }
 }
